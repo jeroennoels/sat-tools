7a268c63b7b9935891dd664571272252b8299e50
jeroennoels <jeroen.noels@telenet.be>
2019-09-18 22:27:14 +0200

    Digits

--- src/Arithmetic.hs
+++ src/Arithmetic.hs
@@ -3,55 +3,32 @@ module Arithmetic where
 import Formula
 import Eval
 import Clauses
+import Digits
 
 import Control.Applicative (liftA2)
 import Data.Maybe
 
--- Plus or Minus
-data Sign i = P1 i | M1 i deriving (Eq, Ord, Show)
 
-plus :: i -> Formula (Sign i)
-plus v = Var (P1 v)
-
-mins :: i -> Formula (Sign i)
-mins v = Var (M1 v)
-
--- The sign of a ternary digit cannot simultaneously be plus and minus.
-isDigit :: i -> Formula (Sign i)
-isDigit v = Not (plus v `And` mins v)
-
-multiplyDigits :: i -> i -> i -> Formula (Sign i)
+multiplyDigits :: IdentifyT1 i j => i -> i -> i -> Formula j
 multiplyDigits a b c =
-  (((plus a `And` plus b) `Or` (mins a `And` mins b)) `Equiv` plus c) `And`
-  (((plus a `And` mins b) `Or` (mins a `And` plus b)) `Equiv` mins c)
+  (((posT1 a `And` posT1 b) `Or` (negT1 a `And` negT1 b)) `Implies` posT1 c) `And`
+  (((posT1 a `And` negT1 b) `Or` (negT1 a `And` posT1 b)) `Implies` negT1 c) `And`
+  ((zeroT1 a `Or` zeroT1 b) `Implies` zeroT1 c)
 
--- A concrete variable assignment that represents one ternary digit
-data Digit1 = Digit1 {isP1 :: Bool, isM1 :: Bool}
-
-getDigit1 :: Eq i => Assignment (Sign i) -> i -> Digit1
-getDigit1 a i = Digit1 {
-  isP1 = assign a (P1 i),
-  isM1 = assign a (M1 i)}
-
-phi1 :: Digit1 -> Maybe Int
-phi1 (Digit1 True False) = Just 1
-phi1 (Digit1 False True) = Just (-1)
-phi1 (Digit1 False False) = Just 0
-phi1 (Digit1 True True) = Nothing  -- forbidden
 
 -- Expressing digits a,b and c with a * b = c
-multiplyABC :: [Clause (Sign Char)]
+multiplyABC :: [Clause (T12 Char)]
 multiplyABC = formulaToClauses $ abcDigits `And` multiplyDigits 'a' 'b' 'c'
   where
-    abcDigits = isDigit 'a' `And` isDigit 'b' `And` isDigit 'c'
+    abcDigits = conjunction $ map isValidT1 "abc"
 
-referenceMultiply :: Digit1 -> Digit1 -> Digit1 -> Maybe Bool
+referenceMultiply :: DigitT1 -> DigitT1 -> DigitT1 -> Maybe Bool
 referenceMultiply a b c = liftA2 (==) (liftA2 (*) (phi1 a) (phi1 b)) (phi1 c)
 
-testMultiplyABC :: Assignment (Sign Char) -> Bool
+testMultiplyABC :: Assignment (T12 Char) -> Bool
 testMultiplyABC assignment = abc == fromMaybe False ref
    where
-     digit = getDigit1 assignment
+     digit = getDigitT1 assignment
      ref = referenceMultiply (digit 'a') (digit 'b') (digit 'c')
      abc = evalClauses multiplyABC assignment
 
