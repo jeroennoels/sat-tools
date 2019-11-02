fb9ab1a1be737c6a9955dc4d8f9430f185255d32
jeroennoels <jeroen.noels@telenet.be>
2019-11-02 15:52:32 +0100

    Explore graph coloring

--- src/Tools.hs
+++ src/Tools.hs
@@ -3,7 +3,7 @@ module Tools where
 
 import Formula
 import Clauses
-import Data.List (concatMap)
+import Data.List (concatMap, nub)
 
 newtype Bit = Bit Int
   deriving (Eq, Ord, Read, Show)
@@ -15,18 +15,28 @@ bitPositions = [Bit 0, Bit 1]
 data VertexColorBit = VertexColorBit Int Bit
   deriving (Eq, Ord, Read, Show)
 
+-- quick hack, assuming 2 bits and thus a priori 4 colors
+onlyThreeColors :: Int -> Formula VertexColorBit
+onlyThreeColors x =
+  Var (VertexColorBit x (Bit 0)) `Or` Var (VertexColorBit x (Bit 1))
+
 differentBits :: Int -> Int -> Bit -> Formula VertexColorBit
 differentBits x y bit = Not $
   Var (VertexColorBit x bit) `Equiv` Var (VertexColorBit y bit)
-  
+
 differentColors :: (Int, Int) -> Formula VertexColorBit
 differentColors (x,y) = disjunction $ map (differentBits x y) bitPositions
-  
+
+vertices :: [(Int, Int)] -> [Int]
+vertices edges = nub (map fst edges ++ map snd edges)
+
 graphColoring :: [(Int, Int)] -> [Clause VertexColorBit]
-graphColoring edges = concatMap (formulaToClauses . differentColors) edges
+graphColoring edges =
+  concatMap (formulaToClauses . differentColors) edges ++
+  concatMap (formulaToClauses . onlyThreeColors) (vertices edges)
 
 graph :: [(Int, Int)]
 graph = completeGraph 4
 
 completeGraph :: Int -> [(Int, Int)]
-completeGraph n = [(x,y) | x <- [1..n], y <- [1..n], x < y]
+completeGraph n = [(x,y) | x <- [1..n], y <- [1..n], x < y
